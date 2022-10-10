ff58613fefe61aae6fe2f9cc5dfc6435fd3a3301
Josh <njmkhari@gmail.com>
2022-10-10 12:44:18 +0200

    At page 35/99

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const express = require('express')
 const app = express()
+const urlprefix = '/api'
+const mongoose = require('mongoose')
+const Fruit = require('./models/fruit')
+const fs = require('fs');
+const cert = fs.readFileSync('keys/certificate.pem')
 
-app.get('/',(req,res)=>
+const options = {
+    server: {sslCA: cert}};
+const connstring = "mongodb+srv://adminjosh:ZX8S3mpL6UHt5ne@cluster0.ecr945g.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(connstring).then(()=>
+{git
+    console.log('Connected :-D')
+}).catch(()=>{
+    console.log('Not Connected:-(')
+}, options);
+
+app.get(urlprefix +'/',(req,res)=>
 {
-    res.send('Hello World Express')
+    res.send('Hello World')
 })
 
-app.get('/test',(req,res)=>
+app.get(urlprefix +'/orders',(req,res)=>
 {
-    res.send('Hello World Express /test')
+    const orders = [
+        {
+            id: "1",
+            name: "Orange"
+        },
+        {
+            id: "2",
+            name: "Banana"
+        },
+        {
+            id: "3",
+            name: "Pear"
+        }
+    ]
+    res.json(
+        {
+            message: "Fruits",
+            orders: orders
+        }
+    )
 })
 
 module.exports = app;
